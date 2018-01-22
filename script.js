238e59b091b8884123dee9a45511583a1794cbb5
kuba-rosiek <rosiek.kuba@gmail.com>
2018-01-22 15:39:24 +0100

    Update script.js
    v2

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("myTopnav");
@@ -8,15 +7,3 @@ function myFunction() {
         x.className = "topnav";
     }
 } 
-
-=======
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-} 
->>>>>>> b0b3b66e1bb3387d547351233385836d33460727
