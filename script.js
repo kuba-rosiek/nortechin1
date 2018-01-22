5ae1d88a2280ccefc86c8da80b99fc57b703c152
kuba-rosiek <rosiek.kuba@gmail.com>
2018-01-22 15:03:54 +0100

    Test
    Test

---
+++ script.js
@@ -0,0 +1,22 @@
+<<<<<<< HEAD
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+} 
+
+=======
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+} 
+>>>>>>> b0b3b66e1bb3387d547351233385836d33460727
